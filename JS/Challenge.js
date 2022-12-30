94ef6947e0f33c6fdee94ff5f2216bbbfb688944
Martina Baez <martinabaez.cc@gmail.com>
2022-12-30 19:26:36 -0300

    challenge

---
+++ JS/Challenge.js
@@ -0,0 +1,18 @@
+const API_url = "https://fakestoreapi.com/products"
+
+const Productos ={
+   
+} 
+
+let btnAdd = 
+ document.getElementById("btnAdd")
+
+let addChart = ( =>(
+	btnAdd.addEventListener("click", addProduct) 
+	))
+
+let chart = document.getElementById("chart")
+	
+function addProduct(){
+		let newProd = document.createElement("div");
+	}
